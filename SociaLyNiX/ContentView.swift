4c5fe8d9fb9bc15631bf9d86e6f95099bd7f0d15
Evan D. Shoup <evan.shoup@gmail.com>
2020-12-28 13:22:10 -0700

    Initial Commit

---
+++ SociaLyNiX/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SociaLyNiX
+//
+//  Created by Evan Shoup on 12/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
